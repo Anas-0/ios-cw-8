401a710432e67a420607f1d74880c83bb5d55252
Anas-0 <anas.almutawa1@gmail.com>
2022-03-12 11:07:07 +0300

    Done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -3,3 +3,13 @@ import UIKit
 // التمرين الأول
 // ClassWork 1
 
+var name = "Anas"
+var birthyear = 2006
+var currentyear = 2022
+var age = currentyear-birthyear
+print("hello my name is \(name) and my age is \(age)")
+
+var num1 = "8"
+var num2 = 4
+var num3 = 2.5
+num3 = Double(num1) ?? 0 + Double(num2)
